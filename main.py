3894c8e6573b04b2c0f72535ef82b0b4a583d965
honor <honornurasyl@gmail.com>
2026-05-14 00:50:13 +0500

    Created main menu interface

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ def main():
 
     cars = FileManager.load_cars()
 
-    for car in cars:
-        print(car)
+    while True:
+
+        print("\n=== Car Rental System ===")
+        print("1. Show cars")
+        print("2. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+
+            for car in cars:
+                print(car)
+
+        elif choice == "2":
+
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option")
 
 
 if __name__ == "__main__":
